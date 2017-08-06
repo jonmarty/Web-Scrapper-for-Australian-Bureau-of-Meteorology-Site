aaab307ab96094a9d278a023d8617ef1a2bf9b7a
jonmarty <jonathan.n.marty@gmail.com>
2017-08-06 13:44:32 -0400

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='BOM',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/jonmarty/Web-Scrapper-for-Australian-Bureau-of-Meteorology-Site',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='Jonathan Marty',
+    author_email='jonathan.n.marty@gmail.com',
+    description='Extract data from the Australian Bureau of Meteorology Site'
+)
